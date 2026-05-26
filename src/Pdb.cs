9f73440753fdcae89ee782235c8d22bdac419f45
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-05-26 14:35:50 -0500

    Consolidate partial classes

--- src/Pdb.cs
+++ src/Pdb.cs
@@ -44,4 +44,28 @@ public static partial class Pdb
 
     public static T Slop<T>(T value, [NotParameterized] int slop) =>
         throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(Slop)));
+
+    public static PdbMoreLikeThisQuery DocumentId(long documentId) =>
+        throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(DocumentId)));
+
+    public static PdbMoreLikeThisQuery Document(string documentJson) =>
+        throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(DocumentId)));
+
+    public static PdbProximityQuery Proximity(string token) =>
+        throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(Proximity)));
+
+    public static PdbProximityQuery ProximityRegex(
+        [StringSyntax(StringSyntaxAttribute.Regex)] string pattern
+    ) => throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(ProximityRegex)));
+
+    public static PdbProximityQuery ProximityRegex(
+        [StringSyntax(StringSyntaxAttribute.Regex)] string pattern,
+        int maxExpansions
+    ) => throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(ProximityRegex)));
+
+    public static PdbProximityQuery ProximityArray(params string[] tokens) =>
+        throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(ProximityArray)));
+
+    public static PdbProximityQuery ProximityArray(params PdbProximityQuery[] operands) =>
+        throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(ProximityArray)));
 }
