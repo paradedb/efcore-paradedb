bb5dc12edf41e30c8bb26d67c00830077ab7e114
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-05-27 14:29:16 -0500

    Remove unnecessary classes

--- src/AllQuery.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace ParadeDB.EntityFrameworkCore;
-
-[ExcludeFromCodeCoverage]
-public abstract class AllQuery : PdbQuery
-{
-    private protected AllQuery() { }
-}
